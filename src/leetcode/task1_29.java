c370827e52673928c5c03fbd4c00d9e29e9d9053
weikeyaodavid <76504079+weikeyaodavid@users.noreply.github.com>
2021-02-01 15:05:00 +0800

    new day

--- src/leetcode/task1_29.java
+++ src/leetcode/task1_29.java
@@ -9,12 +9,13 @@ public class task1_29 {
         Arrays.sort(nums);
         List<List<Integer>> all = new ArrayList<>();
         for (int i = 0; i < nums.length - 2; i++) {
-            if (nums[i + 1] == nums[i] && i != nums.length - 2) continue;
+            if (nums[i + 1] == nums[i]) continue;
             HashSet<Integer> set = new HashSet<>();
             for (int a = i + 1; a < nums.length; a++) {
+                if(i != nums.length - 2 && nums[i + 1] == nums[i])continue;
                 if (set.contains((-nums[i] - nums[a]))) {
                     List<Integer> part = new ArrayList<>();
-                    part.add(nums[i]);
+                    part.add(nums[i])
                     part.add(nums[a]);
                     part.add(-nums[i] - nums[a]);
                     all.add(part);
--- src/leetcode/task2_01.java
+++ src/leetcode/task2_01.java
@@ -1,6 +1,11 @@
 package leetcode;
 
 public class task2_01 {
+    //16
+    public int threeSumClosest(int[] nums, int target) {
+
+        return 0;
+    }
     public static void main(String[] args) {
 
     }
