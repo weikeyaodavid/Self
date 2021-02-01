3ca049e050aa80381aad045bff0295d616bb3da3
weikeyaodavid <76504079+weikeyaodavid@users.noreply.github.com>
2021-02-01 17:28:39 +0800

    add

--- src/leetcode/task1_29.java
+++ src/leetcode/task1_29.java
@@ -8,26 +8,35 @@ public class task1_29 {
     public static List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> all = new ArrayList<>();
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (nums[i + 1] == nums[i]) continue;
-            HashSet<Integer> set = new HashSet<>();
-            for (int a = i + 1; a < nums.length; a++) {
-                if(i != nums.length - 2 && nums[i + 1] == nums[i])continue;
-                if (set.contains((-nums[i] - nums[a]))) {
-                    List<Integer> part = new ArrayList<>();
-                    part.add(nums[i])
-                    part.add(nums[a]);
-                    part.add(-nums[i] - nums[a]);
-                    all.add(part);
-                } else {
-                    set.add(nums[a]);
+        for (int a = 0; a < nums.length - 2; a++) {
+            int b = a + 1;
+            int c = nums.length - 1;
+            if (nums[a] > 0 || nums[c] < 0) return all;
+            if(a > 0 && nums[a] == nums[a - 1]) continue;
+            while (b < c && a < b) {
+                if (nums[a] + nums[b] + nums[c] == 0) {
+                    List<Integer> res = new ArrayList<>();
+                    res.add(nums[a]);
+                    res.add(nums[b]);
+                    res.add(nums[c]);
+                    all.add(res);
+                    while(b < c && nums[b] == nums[++b]);
+                    while(b < c && nums[c] == nums[--c]);
+                }else if (nums[a] + nums[b] + nums[c] < 0) {
+                    while(b < c && nums[b] == nums[++b]);
+                }else if (nums[a] + nums[b] + nums[c] > 0) {
+                    while(b < c && nums[c] == nums[--c]);
                 }
             }
         }
         return all;
     }
+    //       这种写法时间超出限制
+    //       while( nums[b] == nums[b+1] && b < c){
+    //        b++;
+    //        }
 
     public static void main(String[] args) {
-        threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+        System.out.println(threeSum(new int[]{-1,0,1,2,-1,-4}).toString());
     }
 }
