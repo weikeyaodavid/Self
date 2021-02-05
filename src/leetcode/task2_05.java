f2e7ef047ac2608432f056b53bcee039934e46af
weikeyaodavid <76504079+weikeyaodavid@users.noreply.github.com>
2021-02-05 17:43:45 +0800

    Update task2_05.java

--- src/leetcode/task2_05.java
+++ src/leetcode/task2_05.java
@@ -3,8 +3,27 @@ package leetcode;
 public class task2_05 {
 
     //48
+    public void rotate(int[][] matrix) {
+
+    }
 
     public static void main(String[] args) {
 
     }
 }
+
+[1,2,3]        [7,4,1]
+[4,5,6]        [8,5,2]
+[7,8,9]        [9,6,3]
+
+00 02
+01 12
+02 22
+
+10 01
+11 11
+12 21
+
+20 00
+21 10
+22 20
