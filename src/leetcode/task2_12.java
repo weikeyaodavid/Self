380886547b4d87fddae6bf069f9409401f9a5197
weikeyaodavid <76504079+weikeyaodavid@users.noreply.github.com>
2021-02-12 15:08:10 +0800

    log

---
+++ src/leetcode/task2_12.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class task2_12 {
+
+    //54
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int x = matrix[0].length;
+        int y = matrix.length;
+        List<Integer> res = new ArrayList<>();
+        int a = 0;
+        int b = 0;
+        while(){
+            int val = matrix[a][b];
+            if(b < x){
+                b++;
+                continue;
+            }
+        }
+        return null;
+    }
+
+    //59
+    public int[][] generateMatrix(int n) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
