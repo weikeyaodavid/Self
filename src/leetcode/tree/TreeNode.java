329dad546a38df2ad1c9bc4b113fc7fc248c3297
weikeyaodavid <76504079+weikeyaodavid@users.noreply.github.com>
2021-03-03 21:39:51 +0800

    log

--- src/leetcode/tree/TreeNode.java
+++ src/leetcode/tree/TreeNode.java
@@ -12,7 +12,6 @@ public class TreeNode {
     //得到当前树（普通树）最大深度  找到左右子树最大的层数
     public int getHeight(TreeNode node){
         if(node == null)return 0;
-
         int leftTree = getHeight(node.left);
         int rightTree = getHeight(node.right);
         int max = Math.max(leftTree, rightTree);
@@ -22,13 +21,11 @@ public class TreeNode {
     //得到当前树（普通树）最大节点  找到左右子树和本节点的最大值
     public int getMaxVal(TreeNode node){
         if(node == null)return -1;
-
         int rootTree = node.val;
         int leftTree = getMaxVal(node.left);
         int rightTree = getMaxVal(node.right);
         int max = Math.max(leftTree, rightTree);
         max = Math.max(max, rootTree);
-
         return max;
     }
 }
