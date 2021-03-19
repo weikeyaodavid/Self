41887518d6206446694b9f5b4330b50ce8647901
weikeyaodavid <76504079+weikeyaodavid@users.noreply.github.com>
2021-03-19 11:09:55 +0800

    log

---
+++ src/leetcode/backTracking/task3_19.java
@@ -0,0 +1,21 @@
+package leetcode.backTracking;
+
+import java.util.List;
+
+public class task3_19 {
+
+    //90. Subsets II
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+
+        return null;
+    }
+
+
+
+    //79. Word Search
+    public boolean exist(char[][] board, String word) {
+
+        return true;
+    }
+
+}
