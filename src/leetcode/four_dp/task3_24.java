d1533727cc97a6c7e5e63fe9f1610490a737c34a
weikeyaodavid <76504079+weikeyaodavid@users.noreply.github.com>
2021-03-24 18:07:49 +0800

    log

---
+++ src/leetcode/four_dp/task3_24.java
@@ -0,0 +1,26 @@
+package leetcode.four_dp;
+
+public class task3_24 {
+
+    //70. Climbing Stairs
+    public int climbStairs(int n){
+
+        return 0;
+    }
+
+
+    //509. Fibonacci Number
+    //斐波那契数列 当前值只与前两个数相关 所以不需要很长的dp table来记录每一个值
+    public int fib(int n) {
+        if(n < 2)return n;
+        int sum = 0;
+        int pre = 0;
+        int cur = 1;
+        for(int i = 2; i <=n; i++){
+            sum = pre + cur;
+            pre = cur;
+            cur = sum;
+        }
+        return sum;
+    }
+}
