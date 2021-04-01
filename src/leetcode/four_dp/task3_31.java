f2b5d4f28b8b8cb8878d6bff706884d1e44a199f
weikeyaodavid <76504079+weikeyaodavid@users.noreply.github.com>
2021-04-01 16:48:29 +0800

    log

--- src/leetcode/four_dp/task3_31.java
+++ src/leetcode/four_dp/task3_31.java
@@ -4,7 +4,31 @@ public class task3_31 {
 
     //256. Paint House
     public int minCost(int[][] costs) {
+        int[][] dp = new int[costs.length][3];
+        dp[0][0] = costs[0][0];
+        dp[0][1] = costs[0][1];
+        dp[0][2] = costs[0][2];
+        for(int i = 1; i < costs.length; i++){
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + costs[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + costs[i][1];
+            dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][0]) + costs[i][2];
+        }
+        return Math.min(Math.min(dp[costs.length - 1][0], dp[costs.length - 1][1]), dp[costs.length - 1][2]);
+    }
 
 
+    //121. Best Time to Buy and Sell Stock
+    //dynamic programming over time
+    public int maxProfit(int[] prices) {
+        int max =0;
+        int min = prices[0];
+        for(int i = 1; i < prices.length; i++){
+            if(prices[i] < min){
+                min = prices[i];
+            }else {
+                max = Math.max(max, prices[i] - min);
+            }
+        }
+        return max;
     }
 }
---
+++ src/leetcode/four_dp/task4_01.java
@@ -0,0 +1,10 @@
+package leetcode.four_dp;
+
+public class task4_01 {
+
+    //122. Best Time to Buy and Sell Stock II
+    public int maxProfit(int[] prices) {
+
+    }
+
+}
