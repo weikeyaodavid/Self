52f0e490c66c67dac784a38d532d9511b2efeba4
weikeyaodavid <76504079+weikeyaodavid@users.noreply.github.com>
2021-04-15 13:55:21 +0800

    log

--- src/worktest/TreeNode.java
+++ src/worktest/TreeNode.java
@@ -1,9 +1,9 @@
 package worktest;
 
 public class TreeNode {
-    int value;
-    TreeNode left;
-    TreeNode right;
+    public int value;
+    public TreeNode left;
+    public TreeNode right;
 
     public TreeNode(int x) {
         value = x;
