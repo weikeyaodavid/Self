dd183aba4c42d171fddcfd1f155192f42b7b7552
weikeyaodavid <76504079+weikeyaodavid@users.noreply.github.com>
2021-04-17 18:32:54 +0800

    Create task4_17.java

---
+++ src/leetcode/eight_linkedList/task4_17.java
@@ -0,0 +1,11 @@
+package leetcode.eight_linkedList;
+
+import worktest.ListNode;
+
+public class task4_17 {
+
+    //203. Remove Linked List Elements
+    public ListNode removeElements(ListNode head, int val) {
+        
+    }
+}
