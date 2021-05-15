a1a6c526e31b4ca43a4e3b9bd34959dacfd7e018
weikeyaodavid <76504079+weikeyaodavid@users.noreply.github.com>
2021-05-15 17:58:34 +0800

    Update task5_13.java

--- src/leetcode/A_Array/task5_13.java
+++ src/leetcode/A_Array/task5_13.java
@@ -19,4 +19,32 @@ public class task5_13 {
         }
         return left * left > x ? (int)left - 1 : (int)left;
     }
+
+
+    // 912. Sort an Array
+    // QuickSort 快速排序
+    // 不稳定排序 复杂度平均 nlogn
+    // 稳定：冒泡排序
+    public void quickSort(int[] arr, int start, int end){
+        if(start >= end)return;
+        int middle = partition(arr, start, end);
+        partition(arr, start, middle - 1);
+        partition(arr, middle + 1, end);
+    }
+    public int partition(int[] arr, int start, int end) {
+        int pivot = arr[start];
+        int left = start + 1;
+        int right = end;
+        while(left < right){
+            while(left < right && arr[left] <= pivot)left++;
+            while(left < right && arr[right] >= pivot)right--;
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+        }
+        if(left == right && pivot < arr[right])right--;
+        arr[start] = arr[right];
+        arr[right] = pivot;
+        return right;
+    }
 }
