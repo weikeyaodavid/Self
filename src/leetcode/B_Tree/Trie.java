896c56adea62f1615ef718cae37f473d749f62d1
weikeyaodavid <76504079+weikeyaodavid@users.noreply.github.com>
2021-05-19 10:41:11 +0800

    log

---
+++ src/leetcode/B_Tree/Trie.java
@@ -0,0 +1,46 @@
+package leetcode.B_Tree;
+
+public class Trie {
+
+    class TrieNode {
+        boolean end;
+        TrieNode[] children = new TrieNode[26];
+    }
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    TrieNode root;
+
+
+    public void insert(String s) {
+        TrieNode p = root;
+        for(int i = 0; i < s.length(); i++) {
+            int u = s.charAt(i) - 'a';
+            if (p.children[u] == null) p.children[u] = new TrieNode();
+            p = p.children[u];
+        }
+        p.end = true;
+    }
+
+    public boolean search(String s) {
+        TrieNode p = root;
+        for(int i = 0; i < s.length(); i++) {
+            int u = s.charAt(i) - 'a';
+            if (p.children[u] == null) return false;
+            p = p.children[u];
+        }
+        return p.end;
+    }
+
+    public boolean startsWith(String s) {
+        TrieNode p = root;
+        for(int i = 0; i < s.length(); i++) {
+            int u = s.charAt(i) - 'a';
+            if (p.children[u] == null) return false;
+            p = p.children[u];
+        }
+        return true;
+    }
+}
