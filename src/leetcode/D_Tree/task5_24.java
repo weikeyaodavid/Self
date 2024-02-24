43ddcb398d9bd69505ad7a7941b7262acc1e42ab
weikeyaodavid <weikeyao19960701@gmail.com>
2024-02-24 21:00:49 +0800

    more and more

--- src/leetcode/D_Tree/task5_24.java
+++ src/leetcode/D_Tree/task5_24.java
@@ -36,4 +36,27 @@ public class task5_24 {
         node.right = buildTree(queue);
         return node;
     }
+
+    //124. Binary Tree Maximum Path Sum
+    int max = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        dfs(root);
+        return max;
+    }
+    public int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        //计算左边分支最大值，左边分支如果为负数还不如不选择
+        int leftMax = Math.max(0, dfs(root.left));
+        //计算右边分支最大值，右边分支如果为负数还不如不选择
+        int rightMax = Math.max(0, dfs(root.right));
+        //left->root->right 作为路径与已经计算过历史最大值做比较
+        max = Math.max(max, root.val + leftMax + rightMax);
+        // 返回经过root的单边最大分支给当前root的父节点计算使用
+        return root.val + Math.max(leftMax, rightMax);
+    }
 }
